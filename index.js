ecaf8e2082564557251784b6eb87cc36565dff1c
dnyncm <dny.ncm@gmail.com>
2024-11-14 20:02:52 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
-$('h1').css('color', 'red')
+//change css
+$('h1').css('color', 'red');
+// add class = $('h1').addClass('') or remove = .removeClass
+// .hasClass('') check if the selected have that class$('h1').css('color', 'red')
 $('input').keydown(function(e){
     $('h1').text(e.key)
 })
